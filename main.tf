76755e96bfd9264cb372a529f215cd107151dd81
Aditya <aditya.rautaray@navitas.com>
2024-10-29 11:01:44 -0400

    added new file

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "webapp" {
     ami = "ami-06b21ccaeff8cd686"
     associate_public_ip_address = true
     count = 1
-    security_group = "sg-jsshshs"
+    secu_groups = "sg-jsshshs"
     instance_type = "t2.micro"
     tags = {
       "Name" = "webapp"
